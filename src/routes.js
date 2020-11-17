fa19bd5de4d0e7c17fc5d9bf5a7c57670278a86d
Jillian Pomares <pomarj01@gmail.com>
2020-11-17 21:10:07 +0000

    Add dashboard on the route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={Dashboard} />
+        <Route path='/' exact component={Login} />
+        <Route path='/dashboard' exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
