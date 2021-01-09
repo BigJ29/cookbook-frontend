23c054da37c966f623423d5143bfcceb44495461
Jill <pomarj01@gmail.com>
2021-01-09 23:45:22 +0000

    Remove container elam on main app and add new fonts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { Container } from "react-bootstrap";
 import './App.css';
 import Routes from './routes';
 
 
 function App() {
   return (
-    <Container>
-      <div className='App'>
-        <Routes />
-      </div>
-    </Container>
+    <div className='App'>
+      <Routes />
+    </div>
   );
 }
 
