824ebd48ad8b424f26d4bff6d71ad30611d1af8f
Jill <pomarj01@gmail.com>
2021-02-09 16:58:36 +0000

    Added Onboarding route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
+import Onboarding from './pages/Onboarding/Onboarding'
 
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={Dashboard} />
+        <Route path="/onboarding" exact component={Onboarding} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
